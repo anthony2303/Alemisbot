e741eb86e5743c002fe2d9073d0ce4a30f29aae2
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-02-27 09:24:03 -0600

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -1,6 +1,6 @@
 // TheMystic-Bot-MD@BrunoSobrino - _antitoxic.js
 
-const toxicRegex = /puto|puta|rata|estupido|imbecil|rctmre|mrd|verga|vrga|maricon/i;
+const toxicRegex = /puto|puta|pto|hdp|careverga|malparido|malparida|rata|estupido|imbecil|rctmre|mrd|verga|vrga|maricon/i;
 
 export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
   if (m.isBaileys && m.fromMe) {
