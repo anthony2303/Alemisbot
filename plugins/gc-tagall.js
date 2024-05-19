50fede464fd8c5e52920eecd4dc6cb75fe9af7cd
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-05-18 21:28:23 -0600

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -10,7 +10,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   for (const mem of participants) {
     teks += `┣➥ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* *By Anthony Ventas*_\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* *Brysubot*_\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
