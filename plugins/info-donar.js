3c8420d41e129cce230089051d3219e6ff32374e
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-05-18 21:39:18 -0600

    Actualizar info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -1,34 +1,27 @@
-/* ⚠ POR FAVOR NO MODIFIQUES NADA DE AQUÍ ⚠ */
-
-import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
-import fs from 'fs';
-const handler = async (m, {conn, usedPrefix, command}) => {
-  const name = await conn.getName(m.sender);
-  const donar =`
+const handler = async (m, { conn }) => {
+    
+const message = `
 *┏ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
-*┇          「 𝐃𝐎𝐍𝐀𝐑 」*
+*┇          「 PAGAR 」*
 *┣ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
 *┃ 𝙷𝙾𝙻𝙰 ${name}*
 *┃*
 *┃ 👉🏻 𝙰𝚀𝚄𝙸 𝙰𝙻𝙶𝚄𝙽𝙾𝚂 𝙳𝙰𝚃𝙾𝚂*
-*┃ 𝙿𝙾𝚁 𝚂𝙸 𝙶𝚄𝚂𝚃𝙰𝚂 𝙰𝙿𝙾𝚈𝙰𝚁 :𝟹*
+*┃ 𝙿𝙰𝚁𝙰 𝙿𝙰𝙶𝙰𝚁*
 *┃*
 *┃ ➤ TARJETA:* 
-*┃ 4815 1630 6227 4090*
-*┃ ➤ 𝙱𝙰𝙽𝙲𝙾: BBVA* 
-*┃ ➤ 𝙱𝙴𝙽𝙴𝙵𝙸𝙲𝙸𝙰𝚁𝙸𝙾: Antonio Mendivil* 
-*┃ ➤ 𝙲𝙾𝙽𝙲𝙴𝙿𝚃𝙾: APOYO*  
+*┃ 646100146403289125*
+*┃ ➤ 𝙱𝙰𝙽𝙲𝙾: STP* 
+*┃ ➤ 𝙱𝙴𝙽𝙴𝙵𝙸𝙲𝙸𝙰𝚁𝙸𝙾: BRYAN RAMIREZ* 
+*┃ ➤ 𝙲𝙾𝙽𝙲𝙴𝙿𝚃𝙾: APOYO O DONACIÓN*  
 *┃*
 *┃ 👉🏻 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙰𝙼𝙴 𝚂𝙸 𝙽𝙴𝙲𝙴𝚂𝙸𝚃𝙰𝚂*
 *┃ 𝙼𝙰𝚂 𝙳𝙰𝚃𝙾𝚂 𝚈 𝙿𝙰𝚁𝙰 𝙰𝙶𝚁𝙰𝙳𝙴𝙲𝙴𝚁𝚃𝙴*
-*┃ wa.me/525625060788*
-*┗ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*
-`.trim();
-  const aa = {quoted: m, userJid: conn.user.jid};
-  const res = generateWAMessageFromContent(m.chat, {liveLocationMessage: {degreesLatitude: 0, degreesLongitude: 0, caption: donar, secuenceNumber: '0', contextInfo: {mentionedJid: conn.parseMention()}}}, aa);
-  conn.relayMessage(m.chat, res.message, {});
+*┃ wa.me/5219617138507*
+*┗ ┅ ━━━━━━━━━━━━━━━━━ ┅ ━*`.trim()
+    
+conn.sendMessage(m.chat, {text: message}, {quoted: m})    
+    
 };
-handler.help = ['donasi'];
-handler.tags = ['info'];
-handler.command = /^dona(te|si)|donar|apoyar$/i;
+handler.command = /^(Pagar)$/i;
 export default handler;
